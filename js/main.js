88bf8772f0a3b38a2e88f3eacb86bc64d9adaf2e
s0rany <sfc5954@psu.edu>
2026-04-20 12:10:04 -0400

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const restaurants = [
+  { name: "Luigi’s", cuisine: "italian" },
+  { name: "Pasta House", cuisine: "italian" },
+  { name: "El Toro", cuisine: "mexican" },
+  { name: "Taco Spot", cuisine: "mexican" },
+  { name: "Seoul Kitchen", cuisine: "korean" },
+  { name: "Kimchi House", cuisine: "korean" }
+];
